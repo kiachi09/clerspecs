1f8c88e8d4b3b024e039e9a3e5884b50f567e4ca
kiachi09 <dushyantrai1941@gmail.com>
2023-04-23 18:18:45 +0530

    Register reducer done

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,11 +4,15 @@ import {
 	authUser,
 	registerUser,
 	getUserProfile,
+	updateUserProfile,
 } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').post(registerUser);
 router.post('/login', authUser);
-router.route('/profile').get(protect, getUserProfile); // The protect middleware is used as the first argument to protect the getUserProfile
+router
+	.route('/profile')
+	.get(protect, getUserProfile)
+	.put(protect, updateUserProfile); // The protect middleware is used as the first argument to protect the getUserProfile
 
 export default router;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,18 +7,32 @@ import {
 
 import { cartReducer } from './reducers/cartReducer';
 
+import {
+	userLoginReducer,
+	userRegisterReducer,
+	userDetailsReducer,
+} from './reducers/userReducer';
+
 const cartItemsFromStorage = localStorage.getItem('cartItems')
 	? JSON.parse(localStorage.getItem('cartItems'))
 	: [];
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+	? JSON.parse(localStorage.getItem('userInfo'))
+	: null;
+
 const intialState = {
 	cart: { cartItems: cartItemsFromStorage },
+	userLogin: { userInfo: userInfoFromStorage },
 };
 
 const reducer = combineReducers({
 	productList: productListReducer,
 	productDetails: productDetailsReducer,
 	cart: cartReducer,
+	userLogin: userLoginReducer,
+	userRegister: userRegisterReducer,
+	userDetails: userDetailsReducer,
 });
 
 export default configureStore({
