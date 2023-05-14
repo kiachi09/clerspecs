039920dddbe89f44b3544f069a061cb15bb1bbff
kiachi09 <dushyantrai1941@gmail.com>
2023-05-14 22:06:26 +0530

    Fixed the navbar issue

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -6,6 +6,7 @@ import Loader from '../components/Loader';
 import { getUserDetails, updateUserProfile } from '../actions/userActions';
 import { listUserOrders } from '../actions/orderActions';
 import { LinkContainer } from 'react-router-bootstrap';
+import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants';
 
 const ProfileScreen = (history, location) => {
 	const [name, setName] = useState('');
@@ -32,7 +33,8 @@ const ProfileScreen = (history, location) => {
 		if (!userInfo) {
 			history.push('/login');
 		} else {
-			if (!user.name) {
+			if (!user || !user.name || success) {
+				dispatch({ type: USER_UPDATE_PROFILE_RESET });
 				dispatch(getUserDetails('profile'));
 				dispatch(listUserOrders());
 			} else {
@@ -40,7 +42,7 @@ const ProfileScreen = (history, location) => {
 				setEmail(user.email);
 			}
 		}
-	}, [history, userInfo, dispatch, user]);
+	}, [history, userInfo, dispatch, user, success]);
 
 	const submitHandler = e => {
 		e.preventDefault();
